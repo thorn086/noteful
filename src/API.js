fe54372d86ec57642596dcce201369162ee2bf12
Darren Rose <thorn086@gmail.com>
2020-01-19 00:04:27 -0600

    final settings

--- src/API.js
+++ src/API.js
@@ -1,4 +1,4 @@
 export default{
-    API_ENDPOINT: 'https:// polar-river-58308.herokuapp.com/api',
+    API_ENDPOINT: 'https://polar-river-58308.herokuapp.com/api',
     API_KEY: process.env.REACT_APP_API_KEY,
 }
